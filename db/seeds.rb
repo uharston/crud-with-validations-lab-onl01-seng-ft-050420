bddc9e8f68a6f5620209071def03976937d21f2f
uharston <uharston@gmail.com>
2020-07-13 16:52:14 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Song.create(title: "Toosie Slide", released: true, release_year: 2020, artist_name: "Drake", genre: "Rap")
+Song.create(title: "Talisman", artist_name: "Air",release_year: 2007,released: true, genre: "Post-Rock")
+Song.create(      title: 'Rhythms Which I Played',
+release_year: 2000,
+released: true,
+genre: 'Normcore',
+artist_name: 'Man With Instruments')
